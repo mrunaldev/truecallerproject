8fe41ddfc0478dddcb2368a4ed912aef3de5d21d
Mrunal Kanta Muduli <mrunalkanta4u@gmail.com>
2022-12-11 00:29:48 +0530

    test2

--- app.go
+++ app.go
@@ -1,10 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"sync"
+	"time"
 )
 
-func main() {
+var wg sync.WaitGroup
+var PrefixList []string
 
-	fmt.Println("Welcome")
+// Function to find the longest common prefix
+
+func findLongestCommonPrefix(strs []string) string {
+	var longestPrefix string = ""
+	var endPrefix = false
+
+	if len(strs) > 0 {
+		sort.Strings(strs)
+		first := string(strs[0])
+		last := string(strs[len(strs)-1])
+
+		for i := 0; i < len(first); i++ {
+			if !endPrefix && string(last[i]) == string(first[i]) {
+				longestPrefix += string(last[i])
+			} else {
+				endPrefix = true
+			}
+		}
+	}
+	return longestPrefix
+}
+func processLine(line string) {
+	time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+	// fmt.Println("line:", line)
+	PrefixList = append(PrefixList, line)
+	wg.Done()
+}
+func main() {
+	// output := findLongestCommonPrefix([]string{"fayjn", "fayut", "fayume", "fayutom", "dslkgjkls"})
+
+	file := "sample_prefixes.txt"
+	reader, _ := os.Open(file)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		wg.Add(1)
+		go processLine(scanner.Text())
+
+	}
+	wg.Wait()
+	// sort.Strings(PrefixList)
+	fmt.Println(findLongestCommonPrefix(PrefixList))
 }
